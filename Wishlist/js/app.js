f5666f0e2ad99124965241eee801ff536aee2aa1
Francis_BH <francis.bh.4264@gmail.com>
2022-12-03 00:25:56 -0500

    variables del carrito

---
+++ Wishlist/js/app.js
@@ -0,0 +1,4 @@
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito');
+const listaRegalos = document.querySelector('#lista-regalos');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
