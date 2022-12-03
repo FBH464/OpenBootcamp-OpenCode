18cb44bdf2753951598c1cdbb19cad9de505c005
Francis_BH <francis.bh.4264@gmail.com>
2022-12-03 10:45:03 -0500

    construyendo objeto js

--- Wishlist/js/app.js
+++ Wishlist/js/app.js
@@ -1,4 +1,33 @@
 const carrito = document.querySelector('#carrito');
-const contenedorCarrito = document.querySelector('#lista-carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const listaRegalos = document.querySelector('#lista-regalos');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+
+cargarEventListeners();
+function cargarEventListeners() {
+    listaRegalos.addEventListener('click', agregarRegalo);
+}
+
+function agregarRegalo(e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains('agregar-carrito')) {
+        const regaloSeleccionado = e.target.parentElement.parentElement;
+        leerDatosRegalo(regaloSeleccionado);
+    }
+}
+
+//lee contenido y extrae información del html
+function leerDatosRegalo(regaloL) {
+    console.log(regaloL);
+
+    const infoPlanta = {
+        id: regaloL.querySelector('a').getAttribute('data-id'),
+        imagen: regaloL.querySelector('img').src,
+        titulo: regaloL.querySelector('h4').textContent,
+        precio: regaloL.querySelector('span').textContent,
+
+    };
+
+    console.log(infoPlanta);
+}
